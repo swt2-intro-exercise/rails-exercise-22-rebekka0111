b789ed9bde945ca90b8ff25f707923796ede289f
rebekka0111 <rebekkaloh@gmail.com>
2022-11-11 15:58:41 +0100

    test #4

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
-RSpec.describe Author, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe "Author", type: :model do
+  it "should have text inputs for an author's first name, last name, and homepage" do
+      # visit new_author_path
+      author = Author.new
+	author.first_name = "Alan"
+	author.last_name = "Turing"
+	author.homepage = "http://wikipedia.org/Alan_Turing"
+	expect(author.first_name).to eq("Alan")
+	expect(author.last_name).to eq("Turing")
+      expect(author.homepage).to eq("http://wikipedia.org/Alan_Turing")
+      expect(author.name).to eq("Alan Turing")
+  end
 end
