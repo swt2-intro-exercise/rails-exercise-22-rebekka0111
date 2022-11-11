1b8f8e6abc540b88fad7b152d49fd2e1331cdf24
rebekka0111 <rebekkaloh@gmail.com>
2022-11-11 15:49:07 +0100

    test #3

---
+++ spec/features/author/author_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+ describe "Author", type: :feature do
+  it "should have text inputs for an author's first name, last name, and homepage" do
+    # visit new_author_path
+    author = Author.new
+    author.first_name = "Alan"
+    author.last_name = "Turing"
+    author.homepage = "http://wikipedia.org/Alan_Turing"
+    expect(author.first_name).to eq("Alan")
+    expect(author.last_name).to eq("Turing")
+    expect(author.homepage).to eq("http://wikipedia.org/Alan_Turing")
+end
+ end
