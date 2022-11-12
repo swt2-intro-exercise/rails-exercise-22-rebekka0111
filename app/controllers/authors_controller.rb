7340bd545e6b273a550c32fe7f8c744accfcdb12
rebekka0111 <rebekkaloh@gmail.com>
2022-11-12 14:59:58 +0100

    test #5 - 2

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -4,6 +4,7 @@ class AuthorsController < ApplicationController
     def create
         @author = Author.new(params.require(:author).permit(:first_name, :last_name, :homepage))
         @author.save
+        redirect_to root_path, notice: 'Success!'
         redirect_to @author
     end
     def show
