330a56eeed7e3c7d864a8dcd7220003ce8c86238
rebekka0111 <rebekkaloh@gmail.com>
2022-11-13 18:10:52 +0100

    test #10 - 2

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  resources :papers
+  get 'home/index'
+  #resources :papers
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rails routes".
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :authors
+  resources :authors, :papers
   # You can have the root of your site routed with "root"
   root 'home#index'
 
