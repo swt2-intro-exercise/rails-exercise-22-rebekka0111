708a246d58d20a5a24472a5491b682001acef4b0
rebekka0111 <rebekkaloh@gmail.com>
2022-11-15 09:48:24 +0100

    test #13

--- spec/models/paper_spec.rb
+++ spec/models/paper_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
-RSpec.describe Paper, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe "New paper paper", type: :model do
+  it "should have a working constructor" do
+    paper = Paper.new(title: "My Paper", venue: "venue", year: 2022)
+    expect(paper.venue).to eq("venue")
+    expect(paper.title).to eq("My Paper")
+    expect(paper.year).to eq(2022)
+  end
 end
--- spec/views/papers/index.html.erb_spec.rb
+++ spec/views/papers/index.html.erb_spec.rb
@@ -1,5 +1,4 @@
 require 'rails_helper'
-
 RSpec.describe "papers/index", type: :view do
   before(:each) do
     assign(:papers, [
@@ -22,5 +21,20 @@ RSpec.describe "papers/index", type: :view do
     assert_select cell_selector, text: Regexp.new("Title".to_s), count: 2
     assert_select cell_selector, text: Regexp.new("Venue".to_s), count: 2
     assert_select cell_selector, text: Regexp.new(2.to_s), count: 2
+  end
+end
+
+RSpec.describe "mytest", type: :feature do
+  it "should have an link to edit a paper" do
+    @itBook = FactoryBot.create :paper
+
+    visit papers_path
+    expect(page).to have_link('Edit')
+  end
+
+  it "should have an link to delete a paper" do
+    @itBook = FactoryBot.create :paper
+    visit papers_path
+    expect(page).to have_link('Delete')
   end
 end
