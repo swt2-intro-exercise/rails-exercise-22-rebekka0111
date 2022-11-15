54341f9a5e041cfae47aa6cc2c1e3392dfa5cacf
rebekka0111 <rebekkaloh@gmail.com>
2022-11-15 10:18:32 +0100

    test #14

--- app/models/paper.rb
+++ app/models/paper.rb
@@ -1,5 +1,6 @@
 class Paper < ApplicationRecord
-    validates :title, presence: true, length:{minimum: 1}
-    validates :venue, presence: true, length:{minimum: 1}
+    validates :title, :venue, presence: true, length:{minimum: 1}
     validates :year, presence: true, length:{minimum: 1}, numericality:{only_integer: true}
+
+    has_and_belongs_to_many :authors
 end
--- spec/models/paper_spec.rb
+++ spec/models/paper_spec.rb
@@ -7,4 +7,24 @@ describe "New paper paper", type: :model do
     expect(paper.title).to eq("My Paper")
     expect(paper.year).to eq(2022)
   end
+
+  it "should not validate when title, venue or year is empty" do
+    paper1 = Paper.new(title: "My Paper", venue: "venue", year:"" )
+    expect(paper1).to_not be_valid
+    paper2 = Paper.new(title: "My Paper", venue:"" , year: 2022 )
+    expect(paper2).to_not be_valid
+    paper2 = Paper.new(title:"" , venue: "venue", year: 2022 )
+    expect(paper2).to_not be_valid
+  end
+
+  it "should not validate when year is not a numeric value" do
+    paper1 = Paper.new(title: "My Paper", venue: "venue", year: "hallo" )
+    expect(paper1).to_not be_valid
+  end
+
+  it "should have an empty list of authors" do
+    paper = Paper.new(title: "My Paper", venue: "venue", year: 2022)
+    expect(paper.authors).to eq([])
+  end
+
 end
